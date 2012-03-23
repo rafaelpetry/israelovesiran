713083db141f0c776ef6c6a6fc366414a522f373
Vitor Baptista <vbaptis@thoughtworks.com>
2012-03-23 10:02:46 -0400

    Overlay the We (L) Iran logo on the submitted image.

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ require 'sass'
 require 'aws/s3'
 require 'digest/sha1'
 require 'fb_graph'
+require 'RMagick'
 
 configure do
   set :public_folder, Proc.new { File.join(root, "static") }
@@ -19,16 +20,6 @@ configure do
   set :fb_app_secret, ENV['FB_APP_SECRET']
 end
 
-helpers do
-  def callback_url(image_file)
-    'http://' + request.host_with_port + '/facebook_callback/' + image_file
-  end
-
-  def image_url(image_file)
-    settings.s3_website + image_file + '.jpg'
-  end
-end
-
 get '/' do
   haml :index
 end
@@ -38,9 +29,9 @@ post '/upload' do
     redirect '/'
   end
 
-  file_name = Digest::SHA1.hexdigest("#{tempfile.path}#{Time.now}#{Time.now.usec}")
+  image = add_logo(tempfile.path)
   AWS::S3::Base.establish_connection!(:access_key_id => settings.s3_key, :secret_access_key => settings.s3_secret)
-  AWS::S3::S3Object.store(file_name + '.jpg', open(tempfile.path), settings.s3_bucket, :access => :public_read)
+  AWS::S3::S3Object.store(unique_filename + '.jpg', image.to_blob, settings.s3_bucket, :access => :public_read)
 
   redirect "/show/#{file_name}"
 end
@@ -71,3 +62,42 @@ get '/stylesheets/styles.css' do
   content_type 'text/css', :charset => 'utf-8'
   sass :styles
 end
+
+helpers do
+  def callback_url(image_file)
+    'http://' + request.host_with_port + '/facebook_callback/' + image_file
+  end
+
+  def image_url(image_file)
+    settings.s3_website + image_file + '.jpg'
+  end
+
+  def add_logo(image_path)
+    original_image = Magick::Image::read(image_path)[0]
+    user_img = original_image.resize_to_fit(500, 500)
+
+    weloveiran_img = Magick::Image::read('static/images/iran-love-israel-01.png')[0]
+
+    weloveiran_img = resize(user_img, weloveiran_img)
+
+    images = Magick::ImageList.new
+    images << user_img
+    images << weloveiran_img
+
+    max_height = images[0].rows + images[1].rows
+    posy = max_height < 500 ? (images[0].rows - images[1].rows) : (500-images[1].rows)
+
+    images[1].page = Magick::Rectangle.new(images[1].columns, images[1].rows, 0, posy)
+
+    images.flatten_images
+  end
+
+  def resize(image, banner)
+    banner.resize!(image.columns * 0.33, image.rows * 0.33)
+  end
+
+  def unique_filename
+    "#{Digest::SHA1.hexdigest("#{Time.now}#{Time.now.usec}")}.jpg"
+  end
+end
+
