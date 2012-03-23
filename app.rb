3390d1f6c5a13a5af4dfd9feabb2da562c90ba1f
Rafael/Cesar <rafaelpetry+cvortmann@thoughtworks.com>
2012-03-22 21:58:41 -0300

    Added ability to post picture to Facebook.

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require 'haml'
 require 'sass'
 require 'aws/s3'
 require 'digest/sha1'
+require 'fb_graph'
 
 configure do
   set :public_folder, Proc.new { File.join(root, "static") }
@@ -13,6 +14,19 @@ configure do
   set :s3_key, ENV['S3_KEY']
   set :s3_secret, ENV['S3_SECRET']
   set :s3_website, ENV['S3_WEBSITE']
+
+  set :fb_app_id, ENV['FB_APP_ID']
+  set :fb_app_secret, ENV['FB_APP_SECRET']
+end
+
+helpers do
+  def callback_url(image_file)
+    'http://' + request.host_with_port + '/facebook_callback/' + image_file
+  end
+
+  def image_url(image_file)
+    settings.s3_website + image_file + '.jpg'
+  end
 end
 
 get '/' do
@@ -32,7 +46,25 @@ post '/upload' do
 end
 
 get '/show/:image_file' do
-  haml :show, :locals => { :image_url => settings.s3_website + params[:image_file] + '.jpg' }
+  haml :show, :locals => { :image_url => image_url(params[:image_file]), :image_file => params[:image_file] }
+end
+
+get '/share/:image_file' do
+  client = FbGraph::Auth.new(settings.fb_app_id, settings.fb_app_secret).client
+  client.redirect_uri = callback_url(params[:image_file])
+  redirect client.authorization_uri(:scope => [:publish_stream, :publish_actions])
+end
+
+get '/facebook_callback/:image_file' do
+  client = FbGraph::Auth.new(settings.fb_app_id, settings.fb_app_secret).client
+  client.redirect_uri = callback_url(params[:image_file])
+  client.authorization_code = params[:code]
+  token = client.access_token! :client_auth_body
+
+  user = FbGraph::User.me(token)
+  user.photo!(:url => image_url(params[:image_file]), :message => 'Israel Loves Iran')
+
+  redirect "/show/#{params[:image_file]}"
 end
 
 get '/stylesheets/styles.css' do
