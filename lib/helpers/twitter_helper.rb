da4ebe2d09a71c215fea88b9ed34b99c5e7e8351
Cesar Henrique Vortmann <cvortmann@gmail.com>
2012-04-04 11:14:47 -0300

    added via codepink using twitter's api instead of hardcoded

--- lib/helpers/twitter_helper.rb
+++ lib/helpers/twitter_helper.rb
@@ -2,13 +2,13 @@ module Sinatra
   module TwitterHelper
 
     def share_to_twitter_url(photo_url)
-      "https://twitter.com/intent/tweet?source=webclient&text=#{twitter_message(photo_url)}"
+      "https://twitter.com/intent/tweet?source=webclient&text=#{twitter_message(photo_url)}&via=codepink"
     end
 
     private
 
     def twitter_message(photo_url)
-      URI.escape("Iranians, we will never bomb your country. We love you. #{tiny_url(photo_url)} Join me at http://www.weloveiran.org #weloveiran via @codepink")
+      URI.escape("Iranians, we will never bomb your country. We love you. #{tiny_url(photo_url)} Join me at http://www.weloveiran.org #weloveiran")
     end
   end
 
