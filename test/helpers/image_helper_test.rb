813d4ecb08821faba77d30ab325160fbd1fe28f0
Vitor Baptista <vitor@vitorbaptista.com>
2012-04-17 21:21:20 -0400

    Damn you, tests!

--- test/helpers/image_helper_test.rb
+++ test/helpers/image_helper_test.rb
@@ -12,20 +12,26 @@ class ImageHelperTest < Test::Unit::TestCase
     assert use_small_logo?(square)
   end
 
-  def test_use_small_logo_if_photo_is_close_to_a_square
-    square = { :width => 76, :height => 100 }
-
-    assert use_small_logo?(square)
-  end
-
   def test_use_small_logo_if_photo_is_landscape
     square = { :width => 200, :height => 100 }
 
     assert use_small_logo?(square)
   end
 
+  def test_use_small_logo_if_photo_is_close_to_a_square
+    square = { :width => 86, :height => 100 }
+
+    assert use_small_logo?(square)
+  end
+
   def test_use_big_logo_if_photo_is_portrait
     square = { :width => 100, :height => 200 }
+
+    assert_equal false, use_small_logo?(square)
+  end
+
+  def test_use_big_logo_if_photo_is_close_to_a_portrait
+    square = { :width => 85, :height => 100 }
 
     assert_equal false, use_small_logo?(square)
   end
