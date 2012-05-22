7192d26b4e50f2a6f2d2d5fde57f37e145581248
Vitor Baptista <vitor@vitorbaptista.com>
2012-05-22 19:13:36 -0300

    Preview images before uploading.

--- static/javascripts/picture-loader.js
+++ static/javascripts/picture-loader.js
@@ -1,4 +1,19 @@
 $(function() {
+  function handleFileSelect(evt) {
+    var file = evt.target.files[0],
+        reader = new FileReader();
+
+    reader.onload = (function(theFile) {
+      return function (e) {
+        var span = document.createElement('span');
+        span.innerHTML = ['<img class="picture" src="', e.target.result, '" title="', escape(theFile.name), '"/>'].join('');
+        $('#picture').prepend(span);
+      };
+    })(file);
+
+    reader.readAsDataURL(file);
+  };
+
   $('.controls label input[type="radio"]').click(function () {
     $('.controls label').removeClass('active');
     $(this).parent('label').addClass('active');
@@ -6,7 +21,8 @@ $(function() {
     $('body').removeAttr('class');
     $('body').addClass($(this).val());
   });
-  $('input#choose-picture').on("change", function () {
+  $('input#choose-picture').on("change", function (evt) {
+    handleFileSelect(evt);
     $('div.color-picker').show();
     $('div.generate').show();
   });
